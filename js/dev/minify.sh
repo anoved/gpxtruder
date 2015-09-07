ef424ac523eca57f92d24331597b4ebe52bd7d85
Jim DeVona <jim.devona@gmail.com>
2015-09-07 11:14:48 -0400

    Revert "Crude js minification (towards #26)"
    This reverts commit fc74d8058f7b13fa870a21529870afa3e7472a50.
    As implemented, minification is not worth the extra hassle it imposes on testing.

--- js/dev/minify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python -m jsmin csg.js >../csg.js
-python -m jsmin gpxtruder.js >../gpxtruder.js
-python -m jsmin lightgl.js >../lightgl.js
-python -m jsmin openjscad.js >../openjscad.js
-python -m jsmin vincenty.js >../vincenty.js
